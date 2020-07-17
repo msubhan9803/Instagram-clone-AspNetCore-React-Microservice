ca725a065c6bd351b8a0c3853460543d0dc90bba
Mohammad Subhan <mohammadsubhan9803@gmail.com>
2020-07-17 16:42:48 +0500

    Updating

--- src/Instagram.ApiGateway/Startup.cs
+++ src/Instagram.ApiGateway/Startup.cs
@@ -38,7 +38,7 @@ namespace Instagram.ApiGateway
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseRouting();
             app.UseOcelot().Wait();
         }
