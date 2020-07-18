95baa5032bd32e7c1969e0252fbf38e77f40156e
Mohammad Subhan <mohammadsubhan9803@gmail.com>
2020-07-18 17:39:50 +0500

    User & Post Service: Moved ConnectionStrings and jwt configuration from appsettings.json to appsettings.Development.json, Removed Username and Password from ConnectionStrings and secured them in secrets.json in system using user-secrets, add connectionstrings to AppDbContext.cs in OnConfiguring method

--- src/Instagram.Services.User/Domain/Data/AppDbContext.cs
+++ src/Instagram.Services.User/Domain/Data/AppDbContext.cs
@@ -1,27 +1,35 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Binder;
 using Microsoft.Extensions.Configuration.Json;
+using MySql.Data.MySqlClient;
 
 namespace Instagram.Services.User.Data
 {
     public class AppDbContext : DbContext
     {
+        public IConfiguration Configuration { get; }
+        
         public AppDbContext() : base()
         {
 
         }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
         {
+            Configuration = configuration;
 
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json")
-            .Build();
-            optionsBuilder.UseMySql(config.GetConnectionString("DefaultConnection"));
+            var builder = new MySqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            builder.UserID = Configuration["Uid"];
+            builder.Password = Configuration["Password"];
+
+            optionsBuilder.UseMySql(builder.ConnectionString);
         }
 
         public DbSet<Domain.Models.User> Users { get; set; }
