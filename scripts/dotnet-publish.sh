ba441c065e245072f9353f3c8b8237d9130907b8
mranonymous <mohammadsubhan9803@gmail.com>
2020-07-29 23:12:42 +0500

    Restructured directory - src renamed to services, services and .sln file moved to backend

--- scripts/dotnet-publish.sh
+++ scripts/dotnet-publish.sh
@@ -1,4 +1,4 @@
-cd ../src
+cd ../backend/services
 dotnet publish ./Instagram.ApiGateway -c Release -o ./Instagram.ApiGateway/bin/Docker
 dotnet publish ./Instagram.Services.User -c Release -o ./Instagram.Services.User/bin/Docker
 dotnet publish ./Instagram.Services.Post -c Release -o ./Instagram.Services.Post/bin/Docker
